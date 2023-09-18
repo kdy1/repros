8651279f1312221676d6db396815c9906e317356
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-19 01:03:34 +0900

    hmm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use glob::glob;
+use std::path::PathBuf;
+
+fn main() -> Result<()> {
+    {
+        let values = glob("**/__tests__/**")?;
+        dbg!(&values);
+
+        for value in values {
+            dbg!(&value);
+        }
+    }
+
+    {
+        let pattern: Vec<PathBuf> = glob("**/__tests__/**")?.filter_map(|p| p.ok()).collect();
+
+        dbg!(&pattern);
+    }
+
+    Ok(())
 }
